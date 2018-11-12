23ea4dd7e202d1758874b14cadd37273f315973c
tantaizhijun <2228048772@qq.com>
2018-11-12 13:03:22 +0800

    第一次提交, 项目初始化

---
+++ src/main/java/com/spring/blogs/myblogs/MyBlogsApplication.java
@@ -0,0 +1,15 @@
+package com.spring.blogs.myblogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * spring boot 项目启动入口
+ */
+@SpringBootApplication
+public class MyBlogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBlogsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/spring/blogs/myblogs/MyBlogsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.spring.blogs.myblogs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyBlogsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
